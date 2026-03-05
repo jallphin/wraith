a84c6183584a02770905545ad9a5eb5ace6da37d
jallphin <james.allphin@gmail.com>
2026-03-05 16:37:18 +0000

    chore: bump version to v0.3.0

--- cmd/wraith/version.go
+++ cmd/wraith/version.go
@@ -1,6 +1,6 @@
 package main
 
-const Version = "0.2.0"
+const Version = "0.3.0"
 
 // GitCommit is injected at build time via -ldflags.
 // Build with: go build -ldflags "-X main.GitCommit=$(git rev-parse --short HEAD)"
